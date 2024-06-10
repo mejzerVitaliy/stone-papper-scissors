37ef0d6aded604a82ea775b277cdf13dd4f65b69
mejzerVitaliy <mejzervitalik@gmail.com>
2024-06-10 22:34:32 +0300

    игра камень ножницы бумага версия 1

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-// Убедимся, что DOM полностью загружен
-document.addEventListener('DOMContentLoaded', function() {
-    // Получаем элемент кнопки по ID
-    const button = document.getElementById('myButton')
-    const divButton = document.getElementById('divButton')
-    const askToQuestion = document.getElementById('askToQuestion')
-
-    button.addEventListener('click', () => {
-        const newbutton = document.createElement('button')
-        const quiz = document.createElement('h1')
-        quiz.textContent = 'are you gay?'
-        newbutton.textContent = 'yes'
-        divButton.appendChild(quiz)
-        divButton.appendChild(newbutton)
-
-        newbutton.addEventListener('click', () => {
-            const ask = document.createElement('h2')
-            ask.textContent = 'yes, i know, becouse u are going to sambo'
-            askToQuestion.appendChild(ask)
-        })
-    })
-    // button.addEventListener('mouseout', () => {
-    //     alert('mouse on a button')
-    // })
-});
+const start = document.querySelector('button')
+start.addEventListener('click', () => {
+    window.location.href = 'game.html'
+})
