3638d77a5f8dbdb00ced18110758b118d74c721c
mejzerVitaliy <mejzervitalik@gmail.com>
2024-06-11 21:25:52 +0300

    Разработка, день 2

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ const start = document.querySelector('button')
 start.addEventListener('click', () => {
     window.location.href = 'game.html'
 })
+
+const toMenu = document.getElementById('toMenu')
+toMenu.addEventListener('click', () => {
+    window.location.href = 'menu.html'
+})
+
+const rock = document.getElementById('rock')
+const scissors = document.getElementById('scissors')
+const papper = document.getElementById('papper')
+const choice = document.getElementById('choice')
+
+
+choice.addEventListener('click', () => {
+        console.log(rock, scissors, papper)
+    })
+// function choosingVariant() {
+    
+
+    
+// }
