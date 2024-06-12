77a0f7ea6f78905b6e8d40971fd15506984fc6e7
mejzerVitaliy <mejzervitalik@gmail.com>
2024-06-12 19:40:48 +0300

    Разработка Камень ножницы бумага, день 3

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
+
+// -----------------------------------КНОПКИ ИГРАТЬ\МЕНЮ--------------------------------------------
+
+
 const start = document.querySelector('button')
 start.addEventListener('click', () => {
     window.location.href = 'game.html'
 })
+
 
 const toMenu = document.getElementById('toMenu')
 toMenu.addEventListener('click', () => {
     window.location.href = 'menu.html'
 })
 
-const rock = document.getElementById('rock')
-const scissors = document.getElementById('scissors')
-const papper = document.getElementById('papper')
-const choice = document.getElementById('choice')
+// ---------------------------------------ВЫБОР ИГРОКА----------------------------------------------
 
 
-choice.addEventListener('click', () => {
-        console.log(rock, scissors, papper)
+const images = document.querySelectorAll('.variant')
+const pressedVariant = document.querySelector('.imagesOfPlayer')
+
+let lastTarget = null
+pressedVariant.addEventListener('click', (t) => {
+    images.forEach((item) => {
+        item.classList.remove('pressedVariant');
     })
-// function choosingVariant() {
-    
+    if (t.target.className != 'imagesOfPlayer') {
+        t.target.classList.add('pressedVariant');
+        lastTarget = t.target
+    }    
+})
 
-    
-// }
+// --------------------------------------КНОПКА ИГРАТь----------------------------------------------
+
+
+let playerChoice = null
+
+const makeAChoice = document.getElementById('choice')
+makeAChoice.addEventListener('click', () => {
+    playerChoice = lastTarget
+    console.log(playerChoice)
+})
+
+// ---------------------------------------ВЫБОР БОТА------------------------------------------------
+
+
+const botsRock = document.querySelector('.rockBots')
+const botsScissors = document.querySelector('.scissorsBots')
+const botsPapper = document.querySelector('.papperBots')
+
+const arrOfVarriantsBot = [botsRock, botsScissors, botsPapper]
+console.log(arrOfVarriantsBot)
+let i = null
+let indexOfBotChoice = Math.floor(Math.random() * 2.9);
+console.log(indexOfBotChoice)
+
+const botChoice = arrOfVarriantsBot[indexOfBotChoice]
+console.log(botChoice)
+
